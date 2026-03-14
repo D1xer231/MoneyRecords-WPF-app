925dd86c7565695acbc4863e39ef89c728a544d8
Oleksandr <aalexandr397@gmail.com>
2026-03-14 17:05:08 +0000

    Добавьте файлы проекта.

---
+++ MoneyRecords/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MoneyRecords.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoneyRecords
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+            GetLocalTimeDate();
+        }
+
+        public void GetLocalTimeDate()
+        {
+            string Time = DateTime.Now.ToString("t");
+            string Date = DateTime.Now.ToString("dddd dd, yyyy");
+
+            DateTime_TextBlock.Text = $"{Date} {"|"} {Time}";
+        }
+    }
+}
---
+++ MoneyRecords/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyRecords.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? userName = Environment.UserName;
+    }
+}
